169d54a5d2b938a3d386a2a9630523b0bb008157
s.khanov <sekhanov@gmail.com>
2019-07-03 16:21:43 +0300

    lesson4 initial commit

---
+++ geekbrains-spring-2/src/main/java/com/skhanov/geekbrainsspring/flow/WebFlowConfig.java
@@ -0,0 +1,12 @@
+package com.skhanov.geekbrainsspring.flow;
+
+import org.springframework.webflow.config.AbstractFlowConfiguration;
+
+/**
+ * WebFlowConfig
+ */
+public class WebFlowConfig extends AbstractFlowConfiguration {
+ 
+    
+    
+}
