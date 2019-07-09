6ea23378a4c81e865ae19109e51ddecb1d742a2b
s.khanov <sekhanov@gmail.com>
2019-07-09 09:30:00 +0300

    header, script fragmetns

--- geekbrains-spring-2/src/main/java/com/skhanov/geekbrainsspring/flows/order/ProductCart.java
+++ geekbrains-spring-2/src/main/java/com/skhanov/geekbrainsspring/flows/order/ProductCart.java
@@ -2,7 +2,6 @@ package com.skhanov.geekbrainsspring.flows.order;
 
 import java.io.Serializable;
 import java.util.List;
-import java.util.Set;
 
 import org.springframework.web.multipart.MultipartFile;
 
