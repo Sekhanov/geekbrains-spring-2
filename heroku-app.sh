ae78cebef6d1399e745e0256cb47b228adbed0cc
s.khanov <sekhanov@gmail.com>
2019-07-20 07:49:16 +0300

    heroku

--- heroku-app.sh
+++ heroku-app.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
-
+docker login
+heroku login
+heroku container:login
 heroku apps:create sekhanov-my-shop
 heroku addons:create heroku-postgresql:hobby-dev --app sekhanov-my-shop
 heroku config:set DB_DRIVER=org.postgresql.Driver HIBERNATE_DIALECT=org.hibernate.dialect.PostgreSQLDialect --app sekhanov-my-shop
