585274762a82585c75024a1bff3ce138f48b5fbd
Sergey Khanov (home) <sekhanov@gmail.com>
2019-07-21 17:58:08 +0300

    docker-compose shop-up + mysql complete

--- heroku-app.sh
+++ heroku-app.sh
@@ -2,8 +2,8 @@
 docker login
 heroku login
 heroku container:login
-heroku apps:create sekhanov-my-shop
-heroku addons:create heroku-postgresql:hobby-dev --app sekhanov-my-shop
-heroku config:set DB_DRIVER=org.postgresql.Driver HIBERNATE_DIALECT=org.hibernate.dialect.PostgreSQLDialect --app sekhanov-my-shop
-heroku container:push web --app=sekhanov-my-shop
-heroku container:release web --app=sekhanov-my-shop
+heroku apps:create sekhanov-my-shop-ui
+heroku addons:create heroku-postgresql:hobby-dev --app sekhanov-my-shop-ui
+heroku config:set DB_DRIVER=org.postgresql.Driver HIBERNATE_DIALECT=org.hibernate.dialect.PostgreSQLDialect --app sekhanov-my-shop-ui
+heroku container:push web --app=sekhanov-my-shop-ui
+heroku container:release web --app=sekhanov-my-shop-ui
